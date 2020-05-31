b750a2a98b4cc13933b8d64e881067b3aba5967c
Zsombor Szabo <zsombor@gmail.com>
2020-05-31 15:38:36 +0300

    Update default exposure configuration JSON

--- CovidWatch/AppleExposureNotificationApp/LocalStore.swift
+++ CovidWatch/AppleExposureNotificationApp/LocalStore.swift
@@ -102,7 +102,7 @@ public class LocalStore: ObservableObject {
     """
     {"minimumRiskScore":0,
     "attenuationDurationThresholds":[50, 70],
-    "attenuationLevelValues":[1, 1, 1, 8, 8, 8, 8, 8],
+    "attenuationLevelValues":[2, 5, 8, 8, 8, 8, 8, 8],
     "daysSinceLastExposureLevelValues":[1, 2, 2, 4, 6, 8, 8, 8],
     "durationLevelValues":[1, 1, 4, 7, 7, 8, 8, 8],
     "transmissionRiskLevelValues":[0, 3, 6, 8, 8, 6, 0, 6]}
