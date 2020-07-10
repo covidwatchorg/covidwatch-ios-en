71bbd935f1793add8b81d5fe02cb16c928f516e3
Zsombor Szabo <zsombor@gmail.com>
2020-07-10 16:45:04 +0300

    [WIP] Add data structure that supports region selection

---
+++ CovidWatch/Data/CodableRegion.swift
@@ -0,0 +1,12 @@
+//
+//  Created by Zsombor Szabo on 10/07/2020.
+//  Copyright © 2020 Covid Watch. All rights reserved.
+//
+
+import Foundation
+
+public struct CodableRegion: Codable {
+
+    let name: String
+
+}
