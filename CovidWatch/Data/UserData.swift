045f3664a2e52d979f822373828d72e210c63de8
2020Nonprofit <2020charitybrowser@gmail.com>
2020-07-12 03:26:31 -0400

    Added isMenuOpened to UserData

--- CovidWatch/Data/UserData.swift
+++ CovidWatch/Data/UserData.swift
@@ -1,84 +1,79 @@
-//
-//  Created by Zsombor Szabo on 04/05/2020.
-//
-//
+ //
+ //  Created by Zsombor Szabo on 04/05/2020.
+ //
+ //
 
-import Foundation
-import SwiftUI
-import Combine
-import UserNotifications
-import ExposureNotification
+ import Foundation
+ import SwiftUI
+ import Combine
+ import UserNotifications
+ import ExposureNotification
 
-final class UserData: ObservableObject {
+ final class UserData: ObservableObject {
 
-    public static let shared = UserData()
+     public static let shared = UserData()
 
-    @Published(key: "firstRun")
-    var firstRun: Bool = true
+     @Published(key: "firstRun")
+     var firstRun: Bool = true
 
-    @Published(key: "isOnboardingCompleted")
-    var isOnboardingCompleted: Bool = false
+     @Published(key: "isOnboardingCompleted")
+     var isOnboardingCompleted: Bool = false
 
-    @Published(key: "isSetupCompleted")
-    var isSetupCompleted: Bool = false
+     @Published(key: "isSetupCompleted")
+     var isSetupCompleted: Bool = false
+     
+     @Published
+     var isMenuOpened: Bool = false
 
-    @Published(key: "isExposureNotificationSetup")
-    var isExposureNotificationSetup: Bool = false
+     @Published(key: "isExposureNotificationSetup")
+     var isExposureNotificationSetup: Bool = false
 
-    @Published(key: "isUserNotificationsSetup")
-    var isUserNotificationsSetup: Bool = false
+     @Published(key: "isUserNotificationsSetup")
+     var isUserNotificationsSetup: Bool = false
 
-    @Published
-    var showHomeWelcomeMessage: Bool = false
+     @Published
+     var showHomeWelcomeMessage: Bool = false
 
-    @Published
-    var exposureNotificationEnabled: Bool = ExposureManager.shared.manager.exposureNotificationEnabled {
-        didSet {
-            guard exposureNotificationEnabled != oldValue else { return }
+     @Published
+     var exposureNotificationEnabled: Bool = false {
+         didSet {
+             guard exposureNotificationEnabled != oldValue else { return }
 
-            defer {
-                self.configureExposureNotificationStatusMessage()
-            }
+             defer {
+                 self.configureExposureNotificationStatusMessage()
+             }
 
-            guard ENManager.authorizationStatus == .authorized else {
-                if ENManager.authorizationStatus != .unknown {
-                    if self.exposureNotificationEnabled {
-                        withAnimation {
-                            ApplicationController.shared.handleExposureNotificationEnabled(error: ENError(.notAuthorized))
-                            self.exposureNotificationEnabled = false
-                        }
-                    }
-                }
-                return
-            }
+             guard ENManager.authorizationStatus == .authorized else {
+                 if ENManager.authorizationStatus != .unknown {
+                     if self.exposureNotificationEnabled {
+                         withAnimation {
+                             
+                             self.exposureNotificationEnabled = false
+                         }
+                     }
+                 }
+                 return
+             }
+  
+         }
+     }
 
-            ExposureManager.shared.manager.setExposureNotificationEnabled(
-                self.exposureNotificationEnabled
-            ) { (error) in
+     @Published
+     var exposureNotificationStatus: ENStatus = .active {
+         didSet {
+             configureExposureNotificationStatusMessage()
+         }
+     }
 
-                if let error = error {
-                    ApplicationController.shared.handleExposureNotificationEnabled(error: error)
-                    return
-                }
-            }
-        }
-    }
+     func configureExposureNotificationStatusMessage() {
+         self.exposureNotificationStatusMessage =
+             self.exposureNotificationStatus.localizedDetailDescription
+     }
 
-    @Published
-    var exposureNotificationStatus: ENStatus = .active {
-        didSet {
-            configureExposureNotificationStatusMessage()
-        }
-    }
+     @Published
+     var exposureNotificationStatusMessage: String = ""
 
-    func configureExposureNotificationStatusMessage() {
-        self.exposureNotificationStatusMessage =
-            self.exposureNotificationStatus.localizedDetailDescription
-    }
+     @Published
+     var notificationsAuthorizationStatus: UNAuthorizationStatus = .authorized
+ }
 
-    @Published
-    var exposureNotificationStatusMessage: String = ""
-
-    @Published
-    var notificationsAuthorizationStatus: UNAuthorizationStatus = .authorized
-}
