b8c1dae78fc3ab7bb45b629af059eb91e16af83f
2020Nonprofit <2020charitybrowser@gmail.com>
2020-07-11 23:21:20 -0400

    Implemented a background blur view when the menu slides out and fixed a few bugs

--- CovidWatch/Views/Home/Home.swift
+++ CovidWatch/Views/Home/Home.swift
@@ -51,11 +51,11 @@ struct Home: View {
                                     self.userData.exposureNotificationStatus == .disabled {
                                     self.isShowingExposureSettings.toggle()
                                 }
-                                
                             }) {
                                 Alert(
                                     message: userData.exposureNotificationStatus.localizedDetailDescription,
                                     backgroundColor: Color("Alert Standard Color"),
+                                    // swiftlint:disable:next line_length
                                     detailImage: (self.userData.exposureNotificationStatus == .unknown || self.userData.exposureNotificationStatus == .disabled) ? Image("Right Arrow") : nil
                                 )
                             }
@@ -63,10 +63,8 @@ struct Home: View {
                                 Setup1(dismissesAutomatically: true).environmentObject(self.userData)
                             }
                         }
-                        
                         if userData.notificationsAuthorizationStatus != .authorized {
                             Button(action: {
-                                
                                 if self.userData.notificationsAuthorizationStatus == .denied {
                                     guard let settingsUrl = URL(string: UIApplication.openSettingsURLString),
                                         UIApplication.shared.canOpenURL(settingsUrl) else {
@@ -76,10 +74,11 @@ struct Home: View {
                                 } else {
                                     self.isShowingNotificationSettings.toggle()
                                 }
-                                
                             }) {
                                 Alert(
+                                    // swiftlint:disable:next line_length
                                     message: userData.notificationsAuthorizationStatus.localizedDetailDescription,
+                                     // swiftlint:disable:next line_length
                                     backgroundColor: Color("Alert Standard Color")
                                 )
                             }
@@ -87,20 +86,16 @@ struct Home: View {
                                 Setup2(dismissesAutomatically: true).environmentObject(self.userData)
                             }
                         }
-                        
                     }.padding(.top, .headerHeight)
+                        // swiftlint:disable:next line_length
                         .zIndex(1) // Required for the shadow effect to be visible. Otherwise the content the follows below covers it.
-                    
                     ZStack(alignment: .top) {
-                        
                         VStack(spacing: 0) {
-                            
                             Image("Home")
                                 .frame(minWidth: 0, maxWidth: .infinity, alignment: .top)
                                 // swiftlint:disable:next line_length
                                 //                                .background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.263, green: 0.769, blue: 0.851, opacity: 1), Color.white.opacity(0.4)]), startPoint: .top, endPoint: .bottom))
                                 .accessibility(label: Text("HOME_IMAGE_ACCESSIBILITY_LABEL"))
-                            
                             Button(action: {
                                 self.isShowingPossibleExposures.toggle()
                             }) {
@@ -112,14 +107,11 @@ struct Home: View {
                                     .environmentObject(self.userData)
                                     .environmentObject(self.localStore)
                             }
-                            
                             Spacer(minLength: .standardSpacing)
-                            
                             Text("NOTIFY_OTHERS_CALL_TO_ACTION_MESSAGE")
                                 .modifier(SubCallToAction())
                                 .frame(maxWidth: .infinity)
                                 .padding(.horizontal, 2 * .standardSpacing)
-                            
                             Button(action: {
                                 self.isShowingReporting.toggle()
                             }) {
@@ -131,14 +123,12 @@ struct Home: View {
                             .sheet(isPresented: $isShowingReporting) {
                                 Reporting().environmentObject(self.localStore)
                             }
-                            
                             Button(action: {
                                 ApplicationController.shared.handleTapShareApp()
                             }) {
                                 Text("SHARE_THE_APP").modifier(SmallCallToAction())
                             }
                             .padding(.horizontal, 2 * .standardSpacing)
-                            
                             Image("Powered By CW Grey")
                                 .accessibility(label: Text("POWERED_BY_CW_IMAGE_ACCESSIBILITY_LABEL"))
                                 .padding(.top, 2 * .standardSpacing)
@@ -146,19 +136,23 @@ struct Home: View {
                         }
                         // swiftlint:disable:next line_length
                         //                            LinearGradient(gradient: Gradient(colors: [.init(red: 0.263, green: 0.769, blue: 0.851), .init(red: 1, green: 1, blue: 1, opacity: 0.4)]), startPoint: .top, endPoint: .bottom)
+                        // swiftlint:disable:next line_length
                         //                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 10, alignment: .top)
                     }
                 }
             }
-            
             HeaderBarV2()
             if userData.isMenuOpened {
+                BlurView(style: .dark)
+                    .opacity(0.5)
+                    .edgesIgnoringSafeArea(.all)
                 Menu()
                     .environmentObject(self.userData)
                     .environmentObject(self.localStore)
-                    .offset(x: 3 * .standardSpacing, y: 0)
+                    .offset(x: 3.5 * .standardSpacing, y: 0)
                     .transition(.move(edge: .trailing))
                     .zIndex(1)
+                    .edgesIgnoringSafeArea(.all)
             } else {
                 Menu()
                     .environmentObject(self.userData)
@@ -166,6 +160,7 @@ struct Home: View {
                     .offset(x: 12 * .headerHeight, y: 0)
                     .transition(.move(edge: .trailing))
                     .zIndex(1)
+                    .edgesIgnoringSafeArea(.all)
             }
         }
     }
