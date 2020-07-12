3a0ac3e3c3926df505badb924843f84bf435b23c
2020Nonprofit <2020charitybrowser@gmail.com>
2020-07-12 14:43:18 -0400

    Fixed spacing issue

--- CovidWatch/Views/Home/Home.swift
+++ CovidWatch/Views/Home/Home.swift
@@ -75,62 +75,62 @@ struct Home: View {
                             }
                         }
                     }.padding(.top, .headerHeight)
-                    
-                }.padding(.top, .largeHeaderHeight)
-                    .zIndex(1) // Required for the shadow effect to be visible. Otherwise the content the follows below covers it.
-                ZStack(alignment: .top) {
-                    VStack(spacing: 0) {
-                        Image("Home")
-                            .frame(minWidth: 0, maxWidth: .infinity, alignment: .top)
-                            // swiftlint:disable:next line_length
-                            //                                .background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.263, green: 0.769, blue: 0.851, opacity: 1), Color.white.opacity(0.4)]), startPoint: .top, endPoint: .bottom))
-                            .accessibility(label: Text("HOME_IMAGE_ACCESSIBILITY_LABEL"))
-                        Button(action: {
-                            self.isShowingPossibleExposures.toggle()
-                        }) {
-                            PossibleExposureSummary()
-                                .environmentObject(self.localStore)
-                        }
-                        .sheet(isPresented: $isShowingPossibleExposures) {
-                            Test2()
-                                .environmentObject(self.userData)
-                                .environmentObject(self.localStore)
-                        }
-                        Spacer(minLength: .standardSpacing)
-                        Text("NOTIFY_OTHERS_CALL_TO_ACTION_MESSAGE")
-                            .modifier(SubCallToAction())
-                            .frame(maxWidth: .infinity)
-                            .padding(.horizontal, 2 * .standardSpacing)
-                        Button(action: {
-                            self.isShowingReporting.toggle()
-                        }) {
-                            Text("NOTIFY_OTHERS").modifier(SmallCallToAction())
-                        }
-                        .padding(.top, .standardSpacing)
-                        .padding(.bottom, .standardSpacing)
-                        .padding(.horizontal, 2 * .standardSpacing)
-                        .sheet(isPresented: $isShowingReporting) {
-                            Reporting()
-                                .environmentObject(self.localStore)
-                                .environmentObject(self.userData)
-                        }
-                        Button(action: {
-                            ApplicationController.shared.handleTapShareApp()
-                        }) {
-                            Text("SHARE_THE_APP").modifier(SmallCallToAction())
-                        }
-                        .padding(.horizontal, 2 * .standardSpacing)
-                        Image("Powered By CW Grey")
-                            .accessibility(label: Text("POWERED_BY_CW_IMAGE_ACCESSIBILITY_LABEL"))
-                            .padding(.top, 2 * .standardSpacing)
+
+                }.padding(.top, .largeHeaderHeight / 3)
+                        .zIndex(1) // Required for the shadow effect to be visible. Otherwise the content the follows below covers it.
+                    ZStack(alignment: .top) {
+                        VStack(spacing: 0) {
+                            Image("Home")
+                                .frame(minWidth: 0, maxWidth: .infinity, alignment: .top)
+                                // swiftlint:disable:next line_length
+                                //                                .background(LinearGradient(gradient: Gradient(colors: [Color(red: 0.263, green: 0.769, blue: 0.851, opacity: 1), Color.white.opacity(0.4)]), startPoint: .top, endPoint: .bottom))
+                                .accessibility(label: Text("HOME_IMAGE_ACCESSIBILITY_LABEL"))
+                            Button(action: {
+                                self.isShowingPossibleExposures.toggle()
+                            }) {
+                                PossibleExposureSummary()
+                                    .environmentObject(self.localStore)
+                            }
+                            .sheet(isPresented: $isShowingPossibleExposures) {
+                                Test2()
+                                    .environmentObject(self.userData)
+                                    .environmentObject(self.localStore)
+                            }
+                            Spacer(minLength: .standardSpacing)
+                            Text("NOTIFY_OTHERS_CALL_TO_ACTION_MESSAGE")
+                                .modifier(SubCallToAction())
+                                .frame(maxWidth: .infinity)
+                                .padding(.horizontal, 2 * .standardSpacing)
+                            Button(action: {
+                                self.isShowingReporting.toggle()
+                            }) {
+                                Text("NOTIFY_OTHERS").modifier(SmallCallToAction())
+                            }
+                            .padding(.top, .standardSpacing)
                             .padding(.bottom, .standardSpacing)
+                            .padding(.horizontal, 2 * .standardSpacing)
+                            .sheet(isPresented: $isShowingReporting) {
+                                Reporting()
+                                    .environmentObject(self.localStore)
+                                    .environmentObject(self.userData)
+                            }
+                            Button(action: {
+                                 ApplicationController.shared.handleTapShareApp()
+                            }) {
+                                Text("SHARE_THE_APP").modifier(SmallCallToAction())
+                            }
+                            .padding(.horizontal, 2 * .standardSpacing)
+                            Image("Powered By CW Grey")
+                                .accessibility(label: Text("POWERED_BY_CW_IMAGE_ACCESSIBILITY_LABEL"))
+                                .padding(.top, 2 * .standardSpacing)
+                                .padding(.bottom, .standardSpacing)
+                        }
+                        // swiftlint:disable:next line_length
+                        //                            LinearGradient(gradient: Gradient(colors: [.init(red: 0.263, green: 0.769, blue: 0.851), .init(red: 1, green: 1, blue: 1, opacity: 0.4)]), startPoint: .top, endPoint: .bottom)
+                        // swiftlint:disable:next line_length
+                        //                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 10, alignment: .top)
                     }
-                    // swiftlint:disable:next line_length
-                    //                            LinearGradient(gradient: Gradient(colors: [.init(red: 0.263, green: 0.769, blue: 0.851), .init(red: 1, green: 1, blue: 1, opacity: 0.4)]), startPoint: .top, endPoint: .bottom)
-                    // swiftlint:disable:next line_length
-                    //                                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 10, alignment: .top)
-                }
-            } .padding(.top, .headerHeight / 2)
+            }
             VStack(spacing: 0) {
                 HeaderBarV2()
                 ZStack {
@@ -148,8 +148,8 @@ struct Home: View {
                 }
             }
             HeaderBar(showRegionSelection: true)
-                .environmentObject(self.localStore)
-                .environmentObject(self.userData)
+                         .environmentObject(self.localStore)
+                         .environmentObject(self.userData)
             if userData.isMenuOpened {
                 BlurView(style: .dark)
                     .opacity(0.5)
@@ -172,7 +172,7 @@ struct Home: View {
             }
         }
     }
-    
+
 }
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
