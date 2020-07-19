54293b316450c42d0f50361d5412234ca496b167
Zsombor Szabo <zsombor@gmail.com>
2020-07-19 22:56:13 +0300

    Add previous and next buttons to How it Works screens.

--- CovidWatch/Views/Support/PageView.swift
+++ CovidWatch/Views/Support/PageView.swift
@@ -18,7 +18,34 @@ struct PageView<Page: View>: View {
     var body: some View {
         ZStack(alignment: .bottom) {
             PageViewController(controllers: viewControllers, currentPage: $currentPage)
-            PageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+
+            HStack(spacing: 0) {
+                Button(action: {
+                    if self.currentPage > 0 {
+                        self.currentPage -= 1
+                    }
+                }) {
+                    Image("Page Control Left")
+                        .accessibility(label: Text("HOW_IT_WORKS_PAGE_CONTROL_PREVIOUS_BUTTON_ACCESSIBILITY_LABEL"))
+                        .accessibility(hint: Text("HOW_IT_WORKS_PAGE_CONTROL_PREVIOUS_BUTTON_ACCESSIBILITY_HINT"))
+                        .padding(.standardSpacing)
+                }
+                Spacer().frame(width: 74)
+                PageControl(numberOfPages: viewControllers.count, currentPage: $currentPage)
+                Spacer().frame(width: 74)
+                Button(action: {
+                    if self.currentPage < self.viewControllers.count - 1 {
+                        self.currentPage += 1
+                    }
+                }) {
+                    Image("Page Control Right")
+                        .accessibility(label: Text("Next"))
+                        .accessibility(hint: Text("HOW_IT_WORKS_PAGE_CONTROL_NEXT_BUTTON_ACCESSIBILITY_HINT"))
+                        .padding(.standardSpacing)
+                }
+            }
+            .frame(maxWidth: .infinity, alignment: .center)
+            .background(BlurView(style: .systemChromeMaterial).edgesIgnoringSafeArea(.all))
         }
     }
 }
